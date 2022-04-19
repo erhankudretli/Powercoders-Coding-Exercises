d45dc59a3d03d9d0640f0866fc7172317fd763aa
Erhan Kudretli <erhankudretli@gmail.com>
2022-04-19 12:00:30 +0200

    age calculator

---
+++ Week4_Js_Introduction/Age_Calculator_Exercise/app.js
@@ -0,0 +1,8 @@
+let myBirthYear = 1992;
+let aYearinFuture = 2043;
+let myAge = aYearinFuture - myBirthYear;
+let myPossibleAge = myAge - 1;
+
+console.log(
+  "I will be either " + myPossibleAge + " or " + myAge + " in " + aYearinFuture
+);
