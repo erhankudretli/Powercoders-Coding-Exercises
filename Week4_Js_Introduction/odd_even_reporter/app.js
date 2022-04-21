9d2fe443f981fcc367bd84df1d5ab1e1caf49d88
Erhan Kudretli <erhankudretli@gmail.com>
2022-04-21 13:11:06 +0200

    set function

--- Week4_Js_Introduction/odd_even_reporter/app.js
+++ Week4_Js_Introduction/odd_even_reporter/app.js
@@ -1,13 +1,30 @@
 let arr = [];
-for (let index = 1; index < 21; index++) {
-  arr[index - 1] = index;
-}
-console.log(arr);
-let givenNumber = Math.floor(Math.random() * 21) + 1;
+let givenNumber;
+const createNumberArr = () => {
+  for (let index = 1; index < 21; index++) {
+    arr[index - 1] = index;
+  }
+};
+
+const RandomiseNumber = () => {
+  givenNumber = Math.floor(Math.random() * 21) + 1;
+};
 
 const checkGivenNumber = () => {
   if (givenNumber % 2 == 0) {
     console.log("Given Number: " + givenNumber + " = even");
   } else console.log("Given Number: " + givenNumber + " : odd");
 };
-checkGivenNumber();
+
+const showNumberArr = () => {
+  console.log(arr);
+};
+
+const setApp = () => {
+  createNumberArr();
+  showNumberArr();
+  RandomiseNumber();
+  checkGivenNumber();
+};
+
+setApp();
