c341d6837f22b0f4bebba3a0fd25cc00acd47ee0
Erhan Kudretli <erhankudretli@gmail.com>
2022-04-26 15:26:44 +0200

    create a story with the input from user

---
+++ Week5_JS/mad_lib/app.js
@@ -0,0 +1,33 @@
+let myArr = [];
+
+// get the input on CLI i use virtual interface.
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+//i used nested question for saving all the input in a one time.
+readline.question(`What's your name?`, (name) => {
+  readline.question(`What's your verb?`, (vrb) => {
+    readline.question(`What's your adv?`, (adv) => {
+      readline.question(`What's your adjective?`, (adj) => {
+        myArr.push(
+          // to send the inputs into the Array as a Object
+          ` {
+                  name:${name},
+                  verb:${vrb},
+                  adverb:${adv},
+                  adjective:${adj},
+          
+              }
+              `
+        );
+        console.log(myArr[0]);
+
+        readline.close();
+        readline.close();
+        readline.close();
+        readline.close();
+      });
+    });
+  });
+});
